f5f69c6e0456812c86120c05233220e921f47b6d
khariri <khariri5@gmail.com>
2021-01-11 11:05:14 +0700

    perubahan field

--- perubahan_setting_account_res_partner/models/res_partner.py
+++ perubahan_setting_account_res_partner/models/res_partner.py
@@ -2,15 +2,15 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields,models
+from odoo import fields,models
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    property_account_payable =  fields.Many2one(
+    property_account_payable_id =  fields.Many2one(
         required=False,
     )
 
-    property_account_receivable = fields.Many2one(
+    property_account_receivable_id = fields.Many2one(
         required=False,
     )
