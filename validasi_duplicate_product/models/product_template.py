feabc10b099d1e419bed73aa0ef9fa4619852e3f
root <khariri5@gmail.com>
2021-02-01 11:27:05 +0700

    penambahan validasi internal reference

--- validasi_duplicate_product/models/product_template.py
+++ validasi_duplicate_product/models/product_template.py
@@ -11,7 +11,8 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     _sql_constrains = [
-        ('name_uniq','unique(display_name)','Product Name Already Exist!')
+        ('name_uniq','unique(display_name)','Product Name Already Exist!'),
+        ('default_code','unique(default_code)','Internal Reference Already Exist')
     ]
 
     # @api.model_create_multi
@@ -19,8 +20,14 @@ class ProductTemplate(models.Model):
     def create(self, values):
         display_name = values.get('name', False)
         cari_nama_barang = self.env['product.template'].search([('name','=',display_name)])
+        int_reference = values.get('default_code', False)
+        cari_int_reference = self.env['product.template'].search([('default_code','=',int_reference)])
+
         if(len(cari_nama_barang) > 0):
             raise ValidationError(
                 _('Sorry, Product Name Already Exist!'))
-        
+        elif(int_reference != False and len(cari_int_reference) > 0):
+            raise ValidationError(
+                _('Sorry, Internal Reference Already Exist!'))
+
         return super(ProductTemplate, self).create(values)
