fe103817786b7507718e75d8e1c4caa08e9768fc
root <khariri5@gmail.com>
2021-03-09 02:24:23 +0700

    perubahan style font

--- djbc_nofas_pemusnahan/reports/report_xls.py
+++ djbc_nofas_pemusnahan/reports/report_xls.py
@@ -7,18 +7,19 @@ class PemusnahanXlsx(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, lines):
         sheet = workbook.add_worksheet('Pemusnahan')
-        format1 = workbook.add_format({'font_name':'Arial', 'font_size':12, 'align':'center', 'bold':True})
-        format2 = workbook.add_format({'font_name':'Arial', 'font_size':12, 'align':'center'})
+        format1 = workbook.add_format({'font_name':'Arial', 'font_size':13, 'align':'center', 'bold':True})
+        format2 = workbook.add_format({'font_name':'Arial', 'font_size':12, 'left':1, 'bottom':1, 'right':1, 'top':1, 'align':'center'})
+        format_header = workbook.add_format({'font_name':'Arial', 'font_size':12, 'left':1, 'bottom':1, 'right':1, 'top':1, 'align':'center', 'bold':True})
         sheet.merge_range('A1:G1','Laporan Pemusnahan Barang', format1)
         sheet.merge_range('A2:G2','Periode:' + str(data['form']['date_start']) + ' s.d ' + str(data['form']['date_end']), format1)
 
-        sheet.write(3,0,'No', format1)
-        sheet.write(3,1,'Nomor Pemusnahan', format1)
-        sheet.write(3,2,'Tanggal Pemusnahan', format1)
-        sheet.write(3,3,'Kode Barang', format1)
-        sheet.write(3,4,'Nama Barang', format1)
-        sheet.write(3,5,'Jumlah', format1)
-        sheet.write(3,6,'Satuan', format1)
+        sheet.write(3,0,'No', format_header)
+        sheet.write(3,1,'Nomor Pemusnahan', format_header)
+        sheet.write(3,2,'Tanggal Pemusnahan', format_header)
+        sheet.write(3,3,'Kode Barang', format_header)
+        sheet.write(3,4,'Nama Barang', format_header)
+        sheet.write(3,5,'Jumlah', format_header)
+        sheet.write(3,6,'Satuan', format_header)
         i=3
         no=0
         lines = self.env['djbc.nofas_musnah'].search([])
